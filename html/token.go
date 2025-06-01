7db60a32ef2114d4169c3b81fea9538af990f0b7
rumkugel13 <rumkugel13@outlook.com>
2025-06-01 16:47:05 +0200

    remove unused functions

--- html/token.go
+++ html/token.go
@@ -4,29 +4,12 @@ import (
 	"strconv"
 )
 
-type TokenType int
-
-const (
-	TextTokenType TokenType = iota
-	TagTokenType
-)
-
 type Token interface {
-	Value() string
-	Type() TokenType
 	String() string
 }
 
 type TextToken struct {
 	Text string
-}
-
-func (t TextToken) Value() string {
-	return t.Text
-}
-
-func (t TextToken) Type() TokenType {
-	return TextTokenType
 }
 
 func (t TextToken) String() string {
@@ -36,14 +19,6 @@ func (t TextToken) String() string {
 type TagToken struct {
 	Tag string
 	Attributes map[string]string
-}
-
-func (t TagToken) Value() string {
-	return t.Tag
-}
-
-func (t TagToken) Type() TokenType {
-	return TagTokenType
 }
 
 func (t TagToken) String() string {
