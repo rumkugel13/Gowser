18beeb6c784958830914a387683fe218a8ae18f4
rumkugel13 <rumkugel13@outlook.com>
2025-06-02 15:40:49 +0200

    add ability to click links

--- browser/browser.go
+++ browser/browser.go
@@ -31,6 +31,7 @@ type Browser struct {
 	display_list []layout.Command
 	scroll       float32
 	document     *layout.LayoutNode
+	url			 *url.URL
 }
 
 func NewBrowser() *Browser {
@@ -40,15 +41,13 @@ func NewBrowser() *Browser {
 	browser.window = tk9_0.App.Center()
 	tk9_0.Pack(browser.canvas)
 	browser.scroll = 0
-	tk9_0.Bind(tk9_0.App, "<Down>", tk9_0.Command(func() {
-		max_y := max(browser.document.Height+2*layout.VSTEP-DefaultHeight, 0)
-		browser.scroll = min(browser.scroll+SCROLL_STEP, max_y)
-		browser.Draw()
-	}))
+	tk9_0.Bind(tk9_0.App, "<Down>", tk9_0.Command(browser.scrollDown))
+	tk9_0.Bind(tk9_0.App, "<Button-1>", tk9_0.Command(browser.click))
 	return browser
 }
 
 func (b *Browser) Load(url *url.URL) {
+	b.url = url
 	fmt.Println("Requesting URL:", url)
 	start := time.Now()
 	body := url.Request()
@@ -121,6 +120,41 @@ func (b *Browser) links(nodes *html.Node) []string {
 		}
 	}
 	return links
+}
+
+func (b *Browser) scrollDown() {
+	max_y := max(b.document.Height+2*layout.VSTEP-DefaultHeight, 0)
+	b.scroll = min(b.scroll+SCROLL_STEP, max_y)
+	b.Draw()
+}
+
+func (b *Browser) click(e *tk9_0.Event) {
+	x, y := float32(e.X), float32(e.Y)
+	y += b.scroll
+	objs := []*layout.LayoutNode{}
+	for _, obj := range layout.TreeToList(b.document, &[]*layout.LayoutNode{}) {
+		if obj.X <= x && x < obj.X+obj.Width &&
+			obj.Y <= y && y < obj.Y+obj.Height {
+			objs = append(objs, obj)
+		}
+	}
+
+	if len(objs) == 0 {
+		return
+	}
+
+	elt := objs[len(objs)-1].Node
+	for elt != nil {
+		tag, ok := elt.Token.(html.TagToken)
+		if !ok {
+			// pass, text token
+		} else if tag.Tag == "a" && tag.Attributes["href"] != "" {
+			url := b.url.Resolve(tag.Attributes["href"])
+			b.Load(url)
+			return
+		}
+		elt = elt.Parent
+	}
 }
 
 func load_default_style_sheet() {
